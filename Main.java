1d53f1a65587258859bede9c523c535aab7a6cf2
Vitold92 <vitold1992@gmail.com>
2023-07-18 22:39:09 +0200

    Added a fixed code

--- Main.java
+++ Main.java
@@ -3,23 +3,51 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         String[] strings = {"мама", "папа", "їж їжак желе"};
-        Set<Character> uniqueCharacters = findUniqueCharacters(strings);
-        System.out.println(uniqueCharacters);
+        char[] uniqueCharacters = findUniqueCharacters(strings);
+        System.out.println(Arrays.toString(uniqueCharacters));
     }
 
-    public static Set<Character> findUniqueCharacters(String[] strings) {
+    public static char[] findUniqueCharacters(String[] strings) {
         Set<Character> uniqueCharacters = new HashSet<>();
+        int count = 0;
 
-        for (String word : strings) {
-            Set<Character> wordCharacters = new HashSet<>();
+        for (String str : strings) {
+            int[] charCount = new int[Character.MAX_VALUE + 1];
+            boolean allEven = true;
 
-            for (char c : word.toCharArray()) {
-                wordCharacters.add(c);
+            for (char c : str.toCharArray()) {
+                charCount[c]++;
             }
 
-            uniqueCharacters.addAll(wordCharacters);
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if (charCount[c] % 2 != 0) {
+                    allEven = false;
+                    break;
+                }
+            }
+
+            if (allEven) {
+                count++;
+
+                for (char c : str.toCharArray()) {
+                    if (!uniqueCharacters.contains(c)) {
+                        uniqueCharacters.add(c);
+                    }
+                }
+
+                if (count == 2) {
+                    break;
+                }
+            }
         }
 
-        return uniqueCharacters;
+        char[] result = new char[uniqueCharacters.size()];
+        int index = 0;
+        for (char c : uniqueCharacters) {
+            result[index++] = c;
+        }
+
+        return result;
     }
 }
